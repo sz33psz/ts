302cc5cd13f2bc61a633bcb10943165d7aef4b62
sz33psz <sz33psz@gmail.com>
2020-07-03 15:43:54 +0200

    Removed exe files, added -t flag to display ISO8601 timestamps, parse UNIX Timestamp parameter as an override

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ TS - simple timestamp tool:
 `ts 0h0m0s` - print unix timestamp of beginning of current day
 `ts +1d 0h0m0s` - print timestamp of tomorrow's midnight
 `ts 12h30m` - print unix timestamp of 12:30:00 today
+`ts -t` - print current ISO 8601 UTC time
+`ts -t 1590000000` - print ISO 8601 UTC time from unix timestamp
+`ts -t 1590000000 +1d` - print ISO 8601 UTC time from 1 day after the unix timestamp
--- cmd/ts.go
+++ cmd/ts.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/sz33psz/ts"
@@ -12,10 +13,24 @@ func main() {
 	args := os.Args[1:]
 
 	currentTime := time.Now().Unix()
+	printIso := false
 	for _, arg := range args {
+		if arg == "-t" {
+			printIso = true
+			continue
+		}
+		if override, err := strconv.ParseInt(arg, 10, 64); err == nil {
+			currentTime = override
+		}
 		if chg, err := ts.NewChange(arg); err == nil {
 			currentTime = chg.Apply(currentTime)
 		}
 	}
-	fmt.Printf("%v\n", currentTime)
+
+	if printIso {
+		t := time.Unix(currentTime, 0).UTC()
+		fmt.Println(t.Format(time.RFC3339))
+	} else {
+		fmt.Printf("%v\n", currentTime)
+	}
 }
